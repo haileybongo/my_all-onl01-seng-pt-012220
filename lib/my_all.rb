808868e347b85ffe343c0908486b9b1bae23fd86
Hailey Bongo <haileyebongo@gmail.com>
2020-01-31 06:10:00 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,9 @@ require 'pry'
 
 def my_all?(collection)
   i = 0 
+  block_return_values = []
   while i < collection.length 
-  yeild(collection[i])
+  block_return_values << yeild(collection[i])
   i += 1
   end
 end
